1bdb88c376571fc778be718fbbaa861ca9536c6d
Caio de Souza <caiodesouza.cds@gmail.com>
2026-03-19 09:14:11 -0300

    Criada classe abstrata modelo Funcionario

---
+++ src/main/java/com/domain/funcionario/Funcionario.java
@@ -0,0 +1,58 @@
+package com.domain.funcionario;
+
+import java.util.Objects;
+
+public abstract class Funcionario {
+
+    private String nome;
+    private String cpf;
+    private String email;
+    private double salarioBase;
+
+    public Funcionario(String nome, String cpf, String email, double salarioBase) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.salarioBase = salarioBase;
+    }
+
+    public abstract double calcSalario();
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Funcionario that = (Funcionario) o;
+        return Objects.equals(cpf, that.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(cpf);
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", email='" + email + '\'' +
+                ", salarioBase=" + salarioBase +
+                '}';
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+}
